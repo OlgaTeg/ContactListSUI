c641f574c069284f6b287a186d06b166a9352354
OlgaTeg <tegza.o@yandex.ru>
2023-01-24 09:06:54 +0300

    Initial Commit

---
+++ ContactListSUI/ContactListSUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ContactListSUIApp.swift
+//  ContactListSUI
+//
+//  Created by Olga Tegza on 24.01.2023.
+//
+
+import SwiftUI
+
+@main
+struct ContactListSUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
